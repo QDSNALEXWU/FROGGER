6008167ec828eb2b9704febe31d249b7853da1aa
Haoyu Wu <haoyuwu3@illinois.edu>
2017-04-26 22:22:27 -0500

    initial

---
+++ Color_Mapper.sv
@@ -0,0 +1,103 @@
+
+module  color_mapper ( input [0:5] colorcode,
+                       output logic [7:0]  VGA_R, VGA_G, VGA_B );
+
+
+// try to match the color code with the actual RGB values 
+//color code match: 
+//# 0 white  (fffff)
+//# 1 black  (0000)
+//# 2 green (27b212)
+//# 3 red (d80222)
+//# 4 light blue (5db1f0)
+//# 5 yellow (F1FF0A)
+//# 6 grey (b2b2b0)
+//# 7 orange (f27a00)
+//# 8 brown (663300)
+
+
+logic [7:0] Red, Green, Blue;
+assign VGA_R = Red;
+assign VGA_G = Green;
+assign VGA_B = Blue;
+
+
+always_comb
+    begin:RGB_Display
+        
+        // #0 white  (fffff) - 000000 
+        if ((colorcode==6'b000000))
+            begin
+                Red=8'hff;
+                Green=8'hff;
+                Blue=8'hff;
+            end
+        
+        // #1 black  (0000) - 000001
+        else if ((value==6'b000001))
+            begin
+                Red=8'h00;
+                Green=8'h00;
+                Blue=8'h00;
+            end
+        
+        // #2 green (27b212) - 000010    
+        else if ((value==6'b000010))
+            begin
+                Red=8'h27;
+                Green=8'hb2;
+                Blue=8'h12;
+            end
+
+        // #3 red (d80222) - 000011    
+        else if ((value==6'b000011))
+            begin
+                Red=8'hd8;
+                Green=8'h02;
+                Blue=8'h22;
+            end  
+            
+        // #4  light blue (5db1f0) - 000100   
+        else if ((value==6'b000100))
+            begin
+                Red=8'h5d;
+                Green=8'hb1;
+                Blue=8'hf0;
+            end      
+
+        //# 5 yellow (F1FF0A) - 000101    
+        else if ((value==6'b000101))
+            begin
+                Red=8'hF1;
+                Green=8'hFF;
+                Blue=8'h0A;
+            end     
+
+        //# 6 grey (b2b2b0) - 000110      
+        else if ((value==6'b000110))
+            begin
+                Red=8'hb2;
+                Green=8'hb2;
+                Blue=8'hb0;
+            end 
+
+        //# 7 orange (f27a00) - 000111     
+        else if ((value==6'b000111))
+            begin
+                Red=8'hf2;
+                Green=8'h7a;
+                Blue=8'h00;
+            end      
+
+        //# 8 brown (663300) - 001000    
+        else if ((value==6'b001000))
+            begin
+                Red=8'h66;
+                Green=8'h33;
+                Blue=8'h00;
+            end 
+
+
+    end
+endmodule
+
---
+++ frogger_game.sv
@@ -0,0 +1,55 @@
+module	frogger_game ( 	input [9:0] BallX, BallY, 
+									DrawX, DrawY, 
+									BallS
+						input logic [0:15][0:16][0:5] frog_font ;
+    					input logic [0:15][0:24][0:5] firetruck_font ;
+    					input logic [0:13][0:18][0:5] bus_font ;
+    					input logic [0:15][0:22][0:5] motorcycle_font ;
+    					input logic [0:8][0:26][0:5] shortlog_font ;
+    					input logic [0:8][0:49][0:5] mediumlog_font ;
+    					input logic [0:8][0:72][0:5] longlog_font ; 	
+						input Clk,
+						output [0:5] colorcode
+					);
+
+// try to match the color code with the actual RGB values 
+//color code match: 
+//# 0 white  (fffff)
+//# 1 black  (0000)
+//# 2 green (27b212)
+//# 3 red (d80222)
+//# 4 light blue (5db1f0)
+//# 5 yellow (F1FF0A)
+//# 6 grey (b2b2b0)
+//# 7 orange (f27a00)
+//# 8 brown (663300)
+
+
+// const varables used to draw 
+parameter [9:0] frog_width=17;  
+parameter [9:0] frog_height=16;  
+
+parameter [9:0] firetruck_width=25;  
+parameter [9:0] firetruck_height=16;  
+
+parameter [9:0] bus_width=19;  
+parameter [9:0] bus_height=14;  
+
+parameter [9:0] motorcycle_width=16;  
+parameter [9:0] motorcycle_height=23;  
+
+parameter [9:0] log_height=9; 
+parameter [9:0] shortlog_width=27;  
+parameter [9:0] mediumlog_width=50;  
+parameter [9:0] longlog_width=73;  
+
+always_comb
+	begin: decide color code 
+		if( ( DrawX >= BallX) &&  ( DrawX < BallX + frog_width) &&  (DrawY >= BallY ) && (DrawY < BallY + frog_height)  )
+			begin 	
+				colorcode = frog_font[DrawY-BallY][DrawX-BallX]		
+			end 
+		// TODO - add the background stuff		
+	end 
+
+endmodule
